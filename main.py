e9c89ea4da9190f6b34f080a14465b8c3fa7a885
Zavaon Taylor <taylozav000@warren.k12.in.us>
2024-08-28 09:05:29 -0400

    Mathnote

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-frisbees = 3  #pretend we got from user input
+frisbees = 3  
 COST_OF_FRISBEES = 15
-total_cost =45 #YOU DO THIS PART WITH VARIABLES AND MATH OPS
+total_cost =45
 print("The total cost is $", total_cost, sep="45")
